43f5ba51672c266f39174671537f9c03ec3d158e
Kyle Li <likyle001@gmail.com>
2022-10-14 23:14:01 -0400

    working squad

--- players/g3_player.py
+++ players/g3_player.py
@@ -235,12 +235,13 @@ class SpecialForce:
         if len(unit_idxs) > self.team_size:
             self.logger.info(f"SPECIAL FORCE {self.id}: initialized special force team {self.id} with too many units")
 
-    def add_unit(self, unit_idx: str):
+    def add_unit(self, unit_idx: int):
         if len(self.unit_idxs) >= self.team_size:
             self.logger.info(f"SPECIAL FORCE {self.id}: cannot add unit {unit_idx} to special force team {self.id}. Too many units")
             return False
         
         self.unit_idxs.append(unit_idx)
+        print(self.unit_idxs)
         return True
 
     def get_unit_idxs(self):
@@ -306,18 +307,19 @@ class SpecialForce:
             for theta in np.linspace(0, 2 * np.pi, point_count + 1)[0:point_count]:
                 precomp_formation.append([circle_radius * np.cos(theta), circle_radius * np.sin(theta)])
         
-        return precomp_formation
+        return np.array(precomp_formation)
 
     def __compute_formation_positions_around_centroid(self, centroid: np.ndarray):
-        positions = centroid + self.formation
+        positions = np.add(centroid, self.formation)
         return np.clip(positions, 0, 100)
 
     def check_in_formation(self):
         # check if all units in formation
         if len(self.unit_pos) == 0:
             return False
-
-        return self.unit_pos == self.__compute_formation_positions_around_centroid(centroid = self.unit_pos[0])
+        # np.array_equal(self.unit_pos, self.__compute_formation_positions_around_centroid(centroid = self.unit_pos[0]))
+        
+        return np.all(np.absolute(np.subtract(self.unit_pos, self.__compute_formation_positions_around_centroid(centroid = self.unit_pos[0])[0: len(self.unit_pos)])) < [0.01, 0.01])
     
     def __congregate(self):
         # we define target_team_centroid as the centroid of the meeting location of all soldiers
@@ -332,23 +334,27 @@ class SpecialForce:
             else:
                 target_team_centroid = np.array([99, 1])
         else:
-            target_team_centroid = np.clip(np.average(self.unit_pos - self.formation[0:len(self.unit_pos)], axis = 0), 1, 99)
+            target_team_centroid = np.clip(np.average(np.subtract(self.unit_pos, self.formation[0:len(self.unit_pos)]), axis = 0), 1, 99)
         
         self.unit_pos_next_step = self.__compute_formation_positions_around_centroid(centroid = target_team_centroid)[0: len(self.unit_idxs)]
 
     def __attack_target_enemy(self):
-        cur_centroid = self.unit_pos[0]
-        unit_vec_towards_enemy = (self.enemy - cur_centroid) / np.linalg.norm(self.enemy / cur_centroid)
+        cur_centroid = np.array(self.unit_pos[0])
+        unit_vec_towards_enemy = (np.subtract(self.enemy, cur_centroid)) / np.linalg.norm(self.enemy / cur_centroid)
         self.unit_pos_next_step = self.__compute_formation_positions_around_centroid(centroid = cur_centroid + unit_vec_towards_enemy)[0: len(self.unit_idxs)]
 
     def move(self):
         if self.in_formation:
+            print("attacking")
             self.__attack_target_enemy()
         else:
+            print("congregateing")
             self.__congregate()
         
+        if len(self.unit_pos) == 0:
+            return None
         # returns [ [unit_idx, (distance, angle)], ... ]
-        return zip(self.get_unit_idxs, get_moves(self.unit_pos, self.unit_pos_next_step))
+        return zip(self.unit_idxs, get_moves(self.unit_pos, self.unit_pos_next_step[0:len(self.unit_pos)]))
 
     def update_state(self, unit_pos:  np.ndarray):
         assert(len(unit_pos) == len(self.unit_idxs))
@@ -422,9 +428,13 @@ class Player:
         self.sf_units_per_team = max(self.sf_units // 5, 10)
         self.sf_units_per_team = min(self.sf_units_per_team, self.sf_units)
 
+        # SAMPLE SQUAD
+        '''
+        self.special_forces = [SpecialForce(self.logger, self.us, id=i, team_size=13) for i in range(1)]
+        self.special_forces[0].set_target_enemy([20, 25])'''
         # round down sf_units to nearest multiple of sf_units per team
         self.sf_units -= self.sf_units % self.sf_units_per_team
-        self.sf_teams = self.sf_units // self.sf_unit_per_team
+        self.sf_teams = self.sf_units // self.sf_units_per_team
 
     def debug(self, *args):
         self.logger.info(" ".join(str(a) for a in args))
@@ -526,6 +536,32 @@ class Player:
         #    and transform them to List[Tuple[float, float]]
         # 3. things to think about: is it worth the effort to create this system, given what we want to do?
 
+        # SAMPLE SQUAD
+        '''
+        if self.day_n <= 20:
+            self.debug(f'day {self.day_n}: form special forces')
+            special_force_unit_idxs = self.special_forces[0].get_unit_idxs()
+            # print(special_force_unit_idxs)
+            self.special_forces[0].update_state([shapely_pts_to_tuples(unit_pos[self.us])[i] for i in special_force_unit_idxs])
+
+            if not self.special_forces[0].is_team_full():
+                self.special_forces[0].add_unit(unit_idx = self.day_n)
+            
+            special_force_moves = self.special_forces[0].move()
+        
+        else:
+            special_force_moves = self.special_forces[0].move()
+        
+        ret = [(0.0, 0.0) for _ in range(len(unit_pos[self.us]))]
+
+        if special_force_moves != None:
+            for unit_idx, unit_move in special_force_moves:
+                ret[unit_idx] = unit_move
+        else:
+            print("special_force_moves is none")
+        #print(self.special_forces[0].get_unit_idxs())
+        print(ret)
+        return ret'''
         # EARLY GAME: form a 2-layer wall
         if self.day_n <= self.initial_radius:
             self.debug(f'day {self.day_n}: form initial wall')
